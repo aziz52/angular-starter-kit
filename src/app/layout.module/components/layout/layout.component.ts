443b21b98f8e3b1dbd38e210bd3a957a85728229
Aziz Nagpurwala <aziz.nagpurwala@nitorinfotech.com>
2021-07-04 18:10:10 +0530

    Testing lint and husky7

--- src/app/layout.module/components/layout/layout.component.ts
+++ src/app/layout.module/components/layout/layout.component.ts
@@ -19,6 +19,8 @@ export class LayoutComponent implements OnInit {
     console.log("new 7 line added");
     console.log("new 8 line added");
     console.log("new 9 line added");
+    console.log("new 10 line added");
+
 
 
 
